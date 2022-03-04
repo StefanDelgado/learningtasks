1b1e1d9c28cefd61afee4826c49ccb275dac6473
StefanDelgado <99627352+StefanDelgado@users.noreply.github.com>
2022-03-04 11:17:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html>
 	<head>
 		<title>My Home Page</title>
-		</head>
-		<body>
+	</head>
+	<body>
 		<h1>Welcome to My Website! I am Stefan Delgado, Nice to meet you!</h1>
-    <ul>
-		  <li><a href="/index.php">HOME</a></li>
-      <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-    </ul>
-		</body>
+			<ul>
+		 		<li><a href="/index.php">HOME</a></li>
+      				<li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+	  			<li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
+    			</ul>
+	</body>
 	</html>
